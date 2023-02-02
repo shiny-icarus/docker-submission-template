d96ec94017d7a40983e0d8888015647c227eb90b
CamilaGL <camila.garcialam@gmail.com>
2023-02-02 12:34:13 -0300

    Save predictions without image modality number

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ out_path = "/output"
 files = os.listdir( in_path )
 
 for file in files:
+   ofile = file.split("_")[0] + ".nii.gz" #raw filenames are yyyy_zzzz.nii.gz and segmentation files are yyyy.nii.gz
    ipath = os.path.join(in_path, file)
-   opath = os.path.join(out_path, file)
+   opath = os.path.join(out_path, ofile)
    print(ipath, " > ", opath)
    threshold(ipath,opath,1000)
 print("end")
